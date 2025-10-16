2c445bb0595a3273c69d63105148d33288ae58c3
shininghyunho <shininghyunho@uos.ac.kr>
2025-10-16 14:27:53 +0900

    chore : tailwindcss 설치.
    - 공식문서 보면서 간단하게 설정.
    - 이후 next.js 쓸 상황 고려.

--- bangbang/src/SearchBar.tsx
+++ bangbang/src/SearchBar.tsx
@@ -11,12 +11,12 @@ export default function SearchBar() {
 
     return(
         <>
-            <div>
-                <div className="search-field">체크인</div>
-                <div className="search-field">체크아웃</div>
-                <div className="search-field">요금</div>
+            <div className="flex items-center p-2 border rounded-lg shadow-sm">
+                <div className="p-2 font-semibold">체크인</div>
+                <div className="p-2 font-semibold">체크아웃</div>
+                <div className="p-2 text-gray-400">요금</div>
                 <GuestSelector onClick={()=>{setShowModal(true)}}/>
-                <div className="search-field">검색</div>
+                <div className="p-2 bg-blue-500 text-white rounded-md ml-2">검색</div>
             </div>
             <GuestModal isOpen={showModal} onClose={handleClose}></GuestModal>
         </>
