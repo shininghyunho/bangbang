c85086a7f6a982f29ca7e78de3b8ec4d501ffadc
shininghyunho <gusgh3315@uos.ac.kr>
2025-10-24 01:32:01 +0900

    fix : 검색 결과 갯수가 잘못나오던 오류 수정.
    - 서브 쿼리 사용.

--- backend/src/listings/repositories/listing.repository.ts
+++ backend/src/listings/repositories/listing.repository.ts
@@ -18,15 +18,29 @@ export class ListingRepository extends Repository<Listing> {
     const diffTime = Math.abs(endDate.getTime() - startDate.getTime());
     const diffDays = Math.ceil(diffTime / MS_PER_DAY) + 1;
 
+    // 서브쿼리를 사용하여 조건을 만족하는 listingId를 찾습니다.
+    const subQuery = this.createQueryBuilder('listing_sub')
+      .select('listing_sub.id')
+      .innerJoin('listing_sub.schedules', 'schedule_sub')
+      .where('schedule_sub.date BETWEEN :startDate AND :endDate', { startDate, endDate })
+      .andWhere('schedule_sub.isAvailable = :isAvailable', { isAvailable: true })
+      .andWhere('schedule_sub.price BETWEEN :minPrice AND :maxPrice', { minPrice, maxPrice })
+      .groupBy('listing_sub.id')
+      .having('COUNT(DISTINCT schedule_sub.date) = :diffDays', { diffDays });
+
+    // 메인 쿼리에서 서브쿼리의 결과를 사용하여 listing을 필터링하고 schedules를 로드합니다.
     const query = this.createQueryBuilder('listing')
-      .innerJoinAndSelect('listing.schedules', 'schedule') // schedules 관계를 로드
+      .innerJoinAndSelect('listing.schedules', 'schedule')
       .where('listing.guestCapacity >= :guestSize', { guestSize })
       .andWhere('listing.infantCapacity >= :infantSize', { infantSize })
+      .andWhere('listing.id IN (' + subQuery.getQuery() + ')') // 서브쿼리 결과 사용
+      // 메인 쿼리에서도 schedule에 대한 필터링 조건을 추가합니다.
       .andWhere('schedule.date BETWEEN :startDate AND :endDate', { startDate, endDate })
       .andWhere('schedule.isAvailable = :isAvailable', { isAvailable: true })
-      .andWhere('schedule.price BETWEEN :minPrice AND :maxPrice', { minPrice, maxPrice })
-      .groupBy('listing.id, listing.hostId, listing.name, listing.description, listing.guestCapacity, listing.infantCapacity, listing.address')
-      .having('COUNT(DISTINCT schedule.date) = :diffDays', { diffDays });
+      .andWhere('schedule.price BETWEEN :minPrice AND :maxPrice', { minPrice, maxPrice });
+
+    // 서브쿼리의 파라미터를 메인 쿼리에 병합합니다.
+    query.setParameters(subQuery.getParameters());
 
     return query.getMany();
   }
