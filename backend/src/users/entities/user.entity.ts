fc71b7872e52e2da4992a307b40dc522a4bccabb
shininghyunho <gusgh3315@uos.ac.kr>
2025-10-23 17:54:46 +0900

    feat : 유저 Entity 정의.
    - 숙소에서 FK로 참조할수있게 아주 간단하게 구현.

---
+++ backend/src/users/entities/user.entity.ts
@@ -0,0 +1,48 @@
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn,
+  UpdateDateColumn,
+  OneToMany,
+} from 'typeorm';
+
+import { Listing } from '../../listings/listings.entity';
+
+@Entity('users')
+export class User {
+  @PrimaryGeneratedColumn('increment')
+  id: number;
+
+  @Column({ type: 'varchar', length: 255, nullable: false, unique: true })
+  email: string;
+
+  @Column({ type: 'varchar', length: 255, nullable: false })
+  password: string;
+
+  @Column({ type: 'varchar', length: 100, nullable: false })
+  name: string;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+
+  @OneToMany(() => Listing, (listing) => listing.host)
+  listings: Listing[];
+
+  // 이후 구현을 위한 참고 주석.
+  // Relationship with Booking (as user)
+  // @OneToMany(() => Booking, (booking) => booking.user)
+  // bookings: Booking[];
+
+  // Relationship with Role (via UserRole junction table)
+  // @ManyToMany(() => Role)
+  // @JoinTable({
+  //   name: 'user_roles', // junction table name
+  //   joinColumn: { name: 'userId', referencedColumnName: 'id' },
+  //   inverseJoinColumn: { name: 'roleId', referencedColumnName: 'id' },
+  // })
+  // roles: Role[];
+}
