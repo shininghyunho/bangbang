8576056425fa9b5b634485f1de1f2ee0c31c2c3f
shininghyunho <gusgh3315@uos.ac.kr>
2025-10-24 09:46:09 +0900

    fix : docker backend가 실행될 수 있게 의존성 주입 수정.
    - docker-compose backend가 발생하던 오류를 보며 수정.

--- backend/src/listings/listing.module.ts
+++ backend/src/listings/listing.module.ts
@@ -1,14 +1,22 @@
 import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
+import { getDataSourceToken, TypeOrmModule } from '@nestjs/typeorm';
 import { Listing } from './entities/listing.entity';
 import { ListingSchedule } from './entities/listing-schedule.entity';
 import { ListingService } from './listing.service';
 import { ListingController } from './listing.controller';
 import { ListingRepository } from './repositories/listing.repository';
+import { DataSource } from 'typeorm';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Listing, ListingSchedule])],
-  providers: [ListingService, ListingRepository],
+  providers: [
+    ListingService,
+    {
+      provide: ListingRepository,
+      useFactory: (dataSource: DataSource) => new ListingRepository(dataSource),
+      inject: [getDataSourceToken()],
+    },
+  ],
   controllers: [ListingController],
 })
 export class ListingModule {}
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,3 +1,6 @@
+import * as crypto from 'crypto';
+(global as any).crypto = crypto;
+
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
