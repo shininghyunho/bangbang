177bbb662aedb0dd3eda113eff3a580efe318091
shininghyunho <shininghyunho@uos.ac.kr>
2025-10-30 08:46:06 +0900

    design : 헤더 영역 백그라운드 컬러와 패딩 추가.

--- frontend/src/app/Header.tsx
+++ frontend/src/app/Header.tsx
@@ -2,9 +2,9 @@ import SearchBar from "../searchBar/SearchBar";
 
 export function Header() {
   return (
-    <header>
+    <header style={{ backgroundColor: '#FFDDE1', padding: '2rem 0' }}>
       <h1 style={{ textAlign: 'center' }}>여긴 Header 컴포넌트입니다.</h1>
-      <div style={{ display: 'flex', justifyContent: 'center' }}>
+      <div style={{ display: 'flex', justifyContent: 'center', paddingBottom: '1rem' }}>
         <SearchBar />
       </div>
     </header>
