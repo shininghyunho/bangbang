77fed69301f23a335c94a31ca3a3551a23c8d047
shininghyunho <shininghyunho@uos.ac.kr>
2025-10-30 14:09:56 +0900

    feat : kakao 로그인 버튼 기능 추가.
    - REST API KEY는 .env에 저장.
    - redirect url 엔드포인트는 구현전.

--- frontend/src/app/header/Login.tsx
+++ frontend/src/app/header/Login.tsx
@@ -1,12 +1,20 @@
 import { useState } from "react";
 
+const KAKAO_REST_API_KEY = import.meta.env.KAKAO_REST_API_KEY;
+const REDIRECT_URI = "http://localhost:3000/auth/kakao/callback";
+
 export function Login() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   const user = {
     name: '사용자',
     imageUrl: 'https://cataas.com/cat'
   };
-  
+
+  const handleLogin = () => {
+    const kakaoLoginUrl = `https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=${KAKAO_REST_API_KEY}&redirect_uri=${REDIRECT_URI}`;
+    location.href = kakaoLoginUrl;
+  };
+
   return (
     <div style={{ position: 'absolute', top: '1rem', right: '1.5rem', display: 'flex', alignItems: 'center', gap: '1rem' }}>
         {isLoggedIn ? (
@@ -16,7 +24,7 @@ export function Login() {
             <button onClick={() => setIsLoggedIn(false)}>로그아웃</button>
           </>
         ) : (
-          <button onClick={() => setIsLoggedIn(true)}>로그인</button>
+          <button onClick={handleLogin}>로그인</button>
         )}
     </div>
   );
