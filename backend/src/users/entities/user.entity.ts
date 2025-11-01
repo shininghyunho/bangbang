b0ee0714b0f170fb9a0a4481b0a891f551ef3e2d
shininghyunho <shininghyunho@uos.ac.kr>
2025-11-01 16:18:29 +0900

    style : users 모듈 lint.

--- backend/src/users/entities/user.entity.ts
+++ backend/src/users/entities/user.entity.ts
@@ -1,4 +1,4 @@
-import { Column, Entity, Index, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
+import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
 import { OauthAccount } from '../../auth/entities/oauth_account.entity';
 import { Listing } from 'src/listings/entities/listing.entity';
 
--- backend/src/users/repositories/user.repository.ts
+++ backend/src/users/repositories/user.repository.ts
@@ -1,20 +1,20 @@
-import { Injectable } from "@nestjs/common";
-import { InjectRepository } from "@nestjs/typeorm";
-import { Repository } from "typeorm";
-import { User } from "../entities/user.entity";
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from '../entities/user.entity';
 
 @Injectable()
 export class UserRepository {
-    constructor(
-        @InjectRepository(User)
-        private readonly repository: Repository<User>,
-    ) {}
+  constructor(
+    @InjectRepository(User)
+    private readonly repository: Repository<User>,
+  ) {}
 
-    async findById(id: bigint): Promise<User | null> {
-        return this.repository.findOneBy({ id });
-    }
+  async findById(id: bigint): Promise<User | null> {
+    return this.repository.findOneBy({ id });
+  }
 
-    async save(user: User): Promise<User> {
-        return this.repository.save(user);
-    }
+  async save(user: User): Promise<User> {
+    return this.repository.save(user);
+  }
 }
